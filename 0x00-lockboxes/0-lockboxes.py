5ce45b823a62f3f433535594d8cf803f077e89a5
ch-canaza <ch-canaza@hotmail.com>
2021-07-23 09:20:48 -0500

    fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
+"""
+canUnlockAll module
+"""
 
 
 def canUnlockAll(boxes):
-    """canUnlockAll function to check if
-    boxes unlock or not
-    Args:
-        boxes (list): list of boxes (list)
-    Return: False if all boxes cannot unlock and True otherwise
-    """
-    if type(boxes) is not list or not all(type(box) is list for box in boxes):
-        return False
-    if len(boxes) == 0:
-        return False
-    if len(boxes) == 1:
+    num_boxes = len(boxes)
+    keys = [0]
+    for key in keys:
+        key_in_box = boxes[key]
+        for key in key_in_box:
+            if key not in keys and key < num_boxes:
+                keys.append(key)
+    else:
+        if len(keys) < num_boxes:
+            return False
         return True
-    if not boxes[0] and len(boxes) > 1:
-        return False
-    unlock = {k: False for k in range(len(boxes))}
-    unlock[0] = True
-    keys = [key for key in boxes[0]]
-    while keys:
-        new_k = []
-        for key in keys:
-            if key in unlock.keys() and unlock[key] is False:
-                new_k += boxes[key]
-                unlock[key] = True
-        if all(unlock.values()) and len(unlock) == len(boxes):
-            return True
-        keys = new_k
-    return False
-    
