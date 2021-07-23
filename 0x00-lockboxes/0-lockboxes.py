efbe287eb21c7b96e542d17b9815829c334d5a4e
ch-canaza <ch-canaza@hotmail.com>
2021-07-23 09:07:13 -0500

    pre validating boxes type and content

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,6 +6,14 @@ def canUnlockAll(boxes):
     function that determines if all boxes can be opened secuencially
     with keys contained in the other boxes
     """
+    if type(boxes) is not list or not all(type(box) is list for box in boxes):
+        return False
+    if len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
+    if not boxes[0] and len(boxes) > 1:
+        return False
     box_to_open = list(range(1, len(boxes)))
     box_opened = []
     for box in boxes:
